8257558bdfabf2fb4e4fc8c7022d61cb302efce7
David Eriksson <david@activout.se>
2021-01-18 12:26:58 +0100

    Log exception

--- public/setup.php
+++ public/setup.php
@@ -16,6 +16,7 @@ try {
     $setupService->setupStep2($email, $secret);
 } catch (\Exception $e) {
     header("HTTP/1.0 404 Not found");
+    error_log(print_r( $e, true));
     die("Failure! Maybe this link has already been used?");
 }
 echo "Success! Please check your e-mail again!";
